f57b317cbb4f961793fd715fd2b5addb8955b0b2
Sule Mwanzia <mwanziapeter489@gmail.com>
2023-02-08 20:36:03 +0300

    delete files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+import React from "react";
 
-import './App.css';
-import { useState } from 'react'; 
 
 const App =() => {
- const [counter, setCounter] = useState(0); 
-  return (
-    <div className="App">
-      <button>-</button>
-      <h1>0</h1>
-      <button>+</button>
-    </div>
-  );
+    return (
+        <h1>APP</h1>
+    )
 }
 
 export default App;
